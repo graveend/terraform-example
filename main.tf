9095dd37f399b44761f867812609c4753f6b0c48
Geethu Raveendran <geethu.raveendran@genomicsengland.co.uk>
2024-05-15 22:25:14 +0100

    Create lambda

--- main.tf
+++ main.tf
@@ -10,5 +10,50 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-west-2"
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "input" {
+  bucket = "graveend-terraform-example-input"
+}
+
+resource "aws_s3_bucket" "output" {
+  bucket = "graveend-terraform-example-output"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "remove-exif/lambda.py"
+  output_path = "temp/lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "temp/lambda_function_payload.zip"
+  function_name = "remove-exif"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda.handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.10"
 }
